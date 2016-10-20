0d1d71d8114784e5a6fb3ab8084ca1b91d64bc6d
Angel Ortega <an22ortega@gmail.com>
2016-10-20 16:20:31 -0700

    ao - adding d latch_b

---
+++ d_latch_B.v
@@ -0,0 +1,18 @@
+module d_latch_B(clk, q, q_bar, d);
+  output q, q_bar;
+  input clk, d;
+  reg q; 
+  
+  assign q_bar = ~q;
+  
+  
+  always @ (r or s )
+    begin
+      if (clk) // True is any non-zero value
+      begin
+        q <= d;
+      end
+    end
+  end
+  
+endmodule 
