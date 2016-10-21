cb7a398b523c4722e5bef76fa1e8021fca312250
Angel Ortega <an22ortega@gmail.com>
2016-10-20 17:48:27 -0700

    ao - testing some random stuff

--- dff_B.v
+++ dff_B.v
@@ -1,10 +1,13 @@
 module dff_B(clk, q, q_bar, d);
   output q, q_bar;
   input clk, d;
+  reg q;
+  // do I make q a q_bar or use my assign operation
   
   always @ (posedge clk)
   begin
-      
+      q <= d;
+      q_bar <= ~d
   end
 endmodule
     
